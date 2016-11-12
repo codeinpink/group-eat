7fdb550c11e9de5ca704365f9dc612588e04ff70
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-11-12 14:45:25 -0600

    Attempt at hooking models up to database
    not that it works :smiley: :gun:

---
+++ server/boot/create-fake-data.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(app) {
+/*
+    app.dataSources.mysqlDs.automigrate('Restaurant', function(err) {
+        if (err) throw err;
+    });
+
+    app.models.Restaurant.create([
+        {
+            id: '1',
+            name: 'Fake Restaurant',
+            address: {
+                street: '123 Ave',
+                city: 'Cityville',
+                state: 'State',
+                zipcode: '12345'
+            }
+        }
+    ], function(err, restaurants) {
+        if (err) throw err;
+
+        console.log('Models created:\n', restaurants);
+    });
+*/
+};
