6bebff71fe0c73f3e9d031fe6dbe98ae7554b57a
Rain Li <yrl0001@auburn.edu>
2016-11-12 18:10:56 -0600

    commiting before pulling another shannon

--- server/boot/create-fake-data.js
+++ server/boot/create-fake-data.js
@@ -3,31 +3,79 @@
 module.exports = function(app) {
 
 
+  app.dataSources.db.automigrate('restaurant', function(err) {
+
+      if (err) throw err;
+
+  });
+  var fs = require('fs');
+  var obj = fs.readFile('./server/boot/restaurants.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    console.log(obj[0]);
+  });
+}
 
 
-    app.dataSources.db.automigrate('Restaurant', function(err) {
-        if (err) throw err;
-    });
 
-    var fs = require('restaurant.json');
-    var obj = JSON.parse(fs.readFileSync('restaurant.json', 'utf8'));
-    console.log(obj)
-    /*
-    app.models.Restaurant.create([
-        {
-            id: '1',
-            name: 'Fake Restaurant',
-            address: {
-                street: '123 Ave',
-                city: 'Cityville',
-                state: 'State',
-                zipcode: '12345'
-            }
-        }
+/*
+function createChain(app, chainName, chainTags){
+
+    app.models.Chain.create([
+      {
+          name: chainName,
+          tags: chainTags
+      }
     ], function(err, restaurants) {
         if (err) throw err;
-
         console.log('Models created:\n', restaurants);
-    });*/
+
+    });
 
 };
+
+/*
+
+    app.dataSources.mysqlDs.automigrate('Restaurant', function(err) {
+
+        if (err) throw err;
+
+    });s
+
+
+
+    app.models.Restaurant.create([
+
+        {
+
+            id: '1',
+
+            name: 'Fake Restaurant',
+
+            address: {
+
+                street: '123 Ave',
+
+                city: 'Cityville',
+
+                state: 'State',
+
+                zipcode: '12345'
+
+            }
+
+        }
+
+    ], function(err, restaurants) {
+
+        if (err) throw err;
+
+
+
+        console.log('Models created:\n', restaurants);
+
+    });
+
+
+
+};*/
