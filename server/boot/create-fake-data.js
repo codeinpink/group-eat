7a371bfb316ad09d3c895fd6c93a4a7ed2229de3
Rain Li <yrl0001@auburn.edu>
2016-11-12 20:00:13 -0600

    commited before sam's updated restuan js

--- server/boot/create-fake-data.js
+++ server/boot/create-fake-data.js
@@ -1,21 +1,53 @@
 'use strict';
 
 module.exports = function(app) {
-
-
-  app.dataSources.db.automigrate('restaurant', function(err) {
-
+  app.dataSources.db.automigrate('Restaurant', function(err) {
       if (err) throw err;
-
   });
+
   var fs = require('fs');
   var obj = fs.readFile('./server/boot/restaurants.json', 'utf8', function (err, data) {
     if (err) throw err;
     obj = JSON.parse(data);
-    console.log(obj[0]);
+    var allRes = obj.map(function(a) {return a.name;}).sort()
+    //console.log(allRes.sort());
+    console.log(allRes.sort());
+    var chains = getChains(allRes).sort();
+    console.log(allRes.length)
+    console.log(chains.length)
+    for (var res in allRes) {
+      if (!isChain(allRes[res],chains)){
+        //console.log(allRes[res])
+      }
+    }
   });
 }
 
+function loadRestaurants(){
+
+}
+
+
+
+function isChain(name,chainList){
+  return chainList.indexOf(name) > -1;
+
+}
+
+function getChains(allRestaurant){
+  var i,
+  len=allRestaurant.length,
+  result = [],
+  obj = {};
+  for (i=0; i<len; i++)
+  {
+    obj[allRestaurant[i]]=0;
+  }
+  for (i in obj) {
+    result.push(i);
+  }
+  return result;
+}
 
 
 /*
