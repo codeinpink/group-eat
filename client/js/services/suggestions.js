f0292c8ef7113e5dfc6ebd05b0deba21e839dbd8
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-11-13 08:23:20 -0600

    Send list of customer ids

--- client/js/services/suggestions.js
+++ client/js/services/suggestions.js
@@ -1,10 +1,14 @@
 angular.module('app')
     .factory('Suggestions', function($http) {
         function getSuggestions(friends, callback) {
+            var ids = friends.map(function(friend) {
+                return friend.customerId;
+            });
+
             $http({
                 url: 'http://localhost:3000/suggestions/',
                 method: 'GET',
-                params: {friends: friends}
+                params: {friends: ids}
             }).then(function(response) {
                 callback(response.data);
             }, function(response) {
