baa326f41bb469dffaa1dae8c63b08dff25381fb
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-11-13 08:01:26 -0600

    Fix paths

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,10 +1,11 @@
 'use strict';
-var http = require(http)
+var http = require('http');
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/suggestions/', function(req, res) {
-      const suggestion = require('server/boot/suggestion.js');
+      const suggestion = require('../suggestion.js');
       res.send({suggestions: suggestion.getSuggestion(req)});
   })
   server.use(router);
